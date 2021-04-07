d9b5fe161cc295a08a3f10a79e8cf13ae523fb23
Deniz TOKSÖZ <50533886+deniztoksoz@users.noreply.github.com>
2021-04-07 15:38:27 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -4,3 +4,18 @@ while (0>sayi or sayi>10) :
     sayi=int(input("Tek basamaklı bir sayı giriniz : "))
 for i in range(0,sayi,2):
     print(i,end=" ")
+
+#girilen sayı tamsayı mı kontrolü yapan versiyon 2
+while True :
+    try:
+        sayi=int(input("Tek basamaklı bir sayı giriniz : "))
+    except ValueError:
+        print("Lütfen sadece tamsayı girin!")
+        continue
+    if(0<sayi and sayi<10):
+        break
+    else:
+        print("Girdiğiniz sayı tek basamaklı olmadır.")
+
+for i in range(0,sayi,2):
+    print(i,end=" ")
