6ac5260bb2701090cc1551218cc70a3faea0bec3
Deniz TOKSÖZ <50533886+deniztoksoz@users.noreply.github.com>
2021-04-07 15:14:14 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,6 @@
-#Explain your work
-
-#Question 1
-for x in range(a):
-	print(a)
+sayi=int(input("Tek basamaklı bir sayı giriniz : "))
+while (0>sayi or sayi>10) :
+    print("Girdiğiniz sayı tek basamaklı olmadır.")
+    sayi=int(input("Tek basamaklı bir sayı giriniz : "))
+for i in range(0,sayi,2):
+    print(i,end=" ")
