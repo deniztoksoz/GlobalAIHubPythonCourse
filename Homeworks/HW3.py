d15ee2c878366eadf5bd36f515193776160b9fca
Deniz TOKSÖZ <50533886+deniztoksoz@users.noreply.github.com>
2021-04-08 15:29:46 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,77 @@
-#Explain your work
+#Yöntem 1
+kontrol=0
+while True:
+    kadi=input("Kullanıcı Adınızı Giriniz : ")
+    pword=input("Şifrenizi Giriniz : ")
+    if(kadi=="dt" and pword=="123"):
+        print("Sisteme girişiniz sağlandı.")
+        break
+    elif(kadi=="dt" or pword=="123"):
+        print("Kullanıcı adı veya şifrenizi yanlış girdiniz.")
+        kontrol+=1
+        if(kontrol==3):
+            print("3 defa başarısız giriş denemesi yaptınız.\nDaha sonra tekrar deneyiniz.")
+            break
+        continue
+    else:
+        print("Sisteme kayıtlı değilsiniz.")
+        break
+#------------------------------------------------------------------------------------------------------------------
+#Yöntem 2
+kontrol=0
+kadi=input("Kullanıcı Adınızı Giriniz : ")
+if(kadi=="dt"):
+    while True:
+        pword=input("Şifrenizi Giriniz : ")
+        if(pword=="123"):
+            print("Sisteme girişiniz sağlandı.")
+            break
+        else:
+            print("Şifrenizi yanlış girdiniz.")
+            kontrol+=1
+            if(kontrol==3):
+                print("3 defa başarısız giriş denemesi yaptınız.\nDaha sonra tekrar deneyiniz.")
+                break
+            continue
+else:
+    print("Kullanıcı adınızı bulamadık. Sisteme kayıtlı değilsiniz.")
+    print("Önce kayıt oluşturmayı denemelesiniz.")
 
-#Question 1
-for x in range(a):
-	print(a)
+#------------------------------------------------------------------------------------------------------------------
+#Yöntem 3 dict kullanarak kayıt girişi uygulaması. Yeni Kullanıcı Ekleme dahil..
+kulls = {"dt":"123","af":"345","bc":"abcedf"}
+giris_kontrol=True
+while giris_kontrol:
+    kontrol=0
+    kadi=input("Kullanıcı Adınızı Giriniz : ")
+    if(kadi in kulls):
+        while True:
+            pword=input("Şifrenizi Giriniz : ")
+            if(pword==kulls[kadi]):
+                print("Sisteme girişiniz sağlandı.")
+                giris_kontrol=False
+                break
+            else:
+                print("Şifrenizi yanlış girdiniz.")
+                kontrol+=1
+                if(kontrol==3):
+                    print("3 defa başarısız giriş denemesi yaptınız.\nDaha sonra tekrar deneyiniz.")
+                    break
+                continue
+    else:
+        print("Kullanıcı adınızı bulamadık. Sisteme kayıtlı değilsiniz.")
+        cevap=input("Kayıt oluşturmak istiyormusunuz? (e/h)")
+        if cevap=="e":
+            while True:
+                kadi=input("Kullanıcı Adınızı Giriniz : ")
+                if(kadi in kulls):
+                    print("Bu kullanıcı adı kayıtlı. Başka bir ad giriniz.")
+                    continue
+                pword=input("Şifrenizi Giriniz : ")
+                kulls[kadi]=pword
+                print("Şimdi giriş yapabilirsiniz..")
+                break
+        else:
+            print("Sistemimizi ziyaret ettiğiniz için teşekkür ederiz.")
+            break
+
